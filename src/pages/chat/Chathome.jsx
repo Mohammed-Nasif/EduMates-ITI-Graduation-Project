e3647385a50115241874889cd7a793c4b0064ea9
Mohammed Nasif <m.khaled100@ymail.com>
2022-11-07 20:12:55 +0200

    chat version 1

--- src/pages/chat/Chathome.jsx
+++ src/pages/chat/Chathome.jsx
@@ -1,23 +1,25 @@
 import './chathome.scss';
-import { useContext } from 'react';
-import { AuthContext } from './../../context/AuthContext';
+import { Chatnav } from './chatcomponents/Chatnav';
+import { Messages } from './chatcomponents/Messages';
+import { Searchchats } from './chatcomponents/Searchchats';
+import { Chats } from './chatcomponents/Chats';
+import { Input } from './chatcomponents/Input';
 
 export const Chathome = () => {
-	const { currentUser } = useContext(AuthContext);
 	return (
 		<div className='row g-0 gap-5 ps-5 pt-5'>
 			<div className='chat_main col-8 bg-white'>
-				<div className='chat_nav w-100 row'>
-					<div className='overflow-hidden col-3'>
-						<img src={currentUser.photoURL} className='rounded-circle w-25' alt='User Pic' />
-					</div>
-					<div className='col-9'>
-						<h2>{currentUser.displayName}</h2>
-						<p>Active Now</p>
-					</div>
-				</div>
+				<Chatnav />
+				<Messages />
+				<Input />
 			</div>
-			<div className='chat_side col-3 bg-white'>Side</div>
+			<div className='chat_side col-3 bg-white'>
+				<div className='side_head'>
+					<h2 className='side_title'>Your Mates</h2>
+					<Searchchats />
+				</div>
+				<Chats />
+			</div>
 		</div>
 	);
 };
