44bd428e98616c14f347cbf181f028dfbc325b6c
M-Magdy5858 <m.magdy5858@gmail.com>
2022-11-11 14:05:42 +0200

    home page styling and suggests component

--- src/components/topicsToFollow/TopicsToFollow.jsx
+++ src/components/topicsToFollow/TopicsToFollow.jsx
@@ -4,7 +4,7 @@ import { Swiper, SwiperSlide } from 'swiper/react';
 import 'swiper/css';
 import 'swiper/css/pagination';
 import 'swiper/css/navigation';
-import { Autoplay, Pagination, Navigation } from 'swiper';
+import { Autoplay, Navigation } from 'swiper';
 import { Link } from 'react-router-dom';
 //scss
 import './TopicsToFollow.scss';
@@ -16,37 +16,34 @@ export const TopicsToFollow = () => {
 	// console.log(courses)
 	return (
 		<>
-			<div className=' card shadow-sm  topics-suggestion-layout  rounded-4 p-0 m-0'>
-				<h4 className='fw-bold ps-4 fs-6 pt-2'>Discover Our Courses</h4>
+			<div className=" card shadow-sm  topics-suggestion-layout  rounded-4 p-0 m-0">
+				<h4 className="fw-bold ps-3 fs-6 pt-2">Discover Our Courses</h4>
 				<div>
 					<Swiper
 						spaceBetween={30}
 						centeredSlides={true}
+						loop={true}
 						autoplay={{
-							delay: 2500,
+							delay: 3500,
 							disableOnInteraction: false,
 						}}
-						pagination={{
-							clickable: true,
-						}}
-						navigation={true}
-						modules={[Autoplay, Pagination, Navigation]}
-						className='mySwiper'>
+						modules={[Autoplay, Navigation]}
+						className="mySwiper"
+					>
 						{courses.map((course, i) => {
 							return (
-								<SwiperSlide className='' key={i}>
-									<div className='previewCourse p-0 m-0'>
-										<div className='course_img'>
-											<img src={course.courseImg} alt='' className='w-100' />
-											<div className='overlay d-flex justify-content-center align-items-center p-0 m-0'>
+								<SwiperSlide className="" key={i}>
+									<div className="preview-course p-0 m-0">
+										<div className="course_img">
+											<img src={course.courseImg} alt="" className="w-100" />
+											<div className="overlay d-flex justify-content-center align-items-center p-0 m-0">
 												<h4>{course.courseTitle}</h4>
 											</div>
 										</div>
-										<div className='course_details text-center p-1'>
-											<p className='fw-bold'>{course.courseTitle}</p>
-											<Link to='/courses'>
-												{' '}
-												<button className='btn btn-outline-info'>join now</button>
+										<div className="course_details text-center p-1">
+											<p className="fw-bold mb-1">{course.courseTitle}</p>
+											<Link to="/courses">
+												<button className="btn btn-outline-primary px-4 py-1 mb-2">Join Now</button>
 											</Link>
 										</div>
 									</div>
