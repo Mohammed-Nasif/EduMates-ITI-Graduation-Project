1dbd7deec56e25fe0ce68189c6021f14a8f90162
Mohammed Nasif <m.khaled100@ymail.com>
2022-11-12 06:35:08 +0200

    update badges

--- src/components/discussioncomment/Discussioncomment.jsx
+++ src/components/discussioncomment/Discussioncomment.jsx
@@ -1,36 +1,33 @@
 import './discussioncomment.scss';
-import userImage from './../../assets/images/Default-avatar.jpg';
+import Instructor from '../../assets/profileicons/instructor.png';
 import { useEffect } from 'react';
 import { useState } from 'react';
 import { db } from '../../firebase';
-import { arrayRemove, arrayUnion, doc, updateDoc, onSnapshot, deleteDoc, Timestamp, setDoc,query,collection,orderBy } from 'firebase/firestore';
-
-
-
-
+import { onSnapshot, doc } from 'firebase/firestore';
 
 export const Discussioncomment = (props) => {
-    const [commentOwner,setcommentOwner] = useState({})
-    useEffect(()=>{
-        const getOwnerData = onSnapshot(doc(db, 'users', props.comment.createdBy), (doc) => {
-            console.log(doc.data())
-            setcommentOwner(doc.data());
-        });
-        return () => {
+	const [commentOwner, setcommentOwner] = useState({});
+	useEffect(() => {
+		const getOwnerData = onSnapshot(doc(db, 'users', props.comment.createdBy), (doc) => {
+			console.log(doc.data());
+			setcommentOwner(doc.data());
+		});
+		return () => {
 			getOwnerData();
 		};
-    },[props.comment])
-    return (
-        <div className="discussion-comment mb-1 pt-1 pb-3 mx-auto ps-0">
-            <div className='row p-3 comment d-flex align-items-baseline gx-0'>
-                <div className='col-2 text-center pe-0'>
-                    <img src={commentOwner.photoURL} alt="" />
-                </div>
-                <div className='col-5 d-flex align-items-center flex-column'>
-                    <p className='mb-1 w-100 fw-bold user-name'>{commentOwner.displayName}</p>
-                </div>
-            </div>
-            <p className='w-100 mb-0 ps-5 ms-4 comment-content'>{props.comment.commentContent}</p>
-        </div>
-    )
-}
+	}, [props.comment]);
+	return (
+		<div className='discussion-comment mb-1 pt-1 pb-3 mx-auto ps-0'>
+			<div className='row p-3 comment d-flex align-items-baseline gx-0'>
+				<div className='col-2 text-center pe-0'>
+					<img src={commentOwner.photoURL} alt='' />
+				</div>
+				<div className='col-5 d-flex align-items-center flex-column'>
+					<p className='mb-1 w-100 fw-bold user-name'>{commentOwner.displayName}</p>
+					{commentOwner?.specialFlags?.isInstructor && <img className='inst-badge' src={Instructor} alt='Instructor' title='Instructor' />}
+				</div>
+			</div>
+			<p className='w-100 mb-0 ps-5 ms-4 comment-content'>{props.comment.commentContent}</p>
+		</div>
+	);
+};
--- src/components/discussionpost/Discussionpost.jsx
+++ src/components/discussionpost/Discussionpost.jsx
@@ -1,204 +1,195 @@
 import './discussionpost.scss';
-import userImage from './../../assets/images/Default-avatar.jpg';
 import { useState, useRef, useContext } from 'react';
-import {AiOutlineSend} from "react-icons/ai";
-import {BsArrowReturnRight} from "react-icons/bs";
-import { Discussioncomment } from './../discussioncomment/Discussioncomment'
+import { AiOutlineSend } from 'react-icons/ai';
+import { BsArrowReturnRight } from 'react-icons/bs';
+import { Discussioncomment } from './../discussioncomment/Discussioncomment';
+import Instructor from '../../assets/profileicons/instructor.png';
 
 import { useEffect } from 'react';
 import { db } from '../../firebase';
-import { arrayRemove, arrayUnion, doc, updateDoc, onSnapshot, deleteDoc, Timestamp, setDoc,query,collection,orderBy } from 'firebase/firestore';
+import { arrayUnion, doc, updateDoc, onSnapshot, Timestamp } from 'firebase/firestore';
 import { AuthContext } from '../../context/AuthContext';
 import { v4 as uuid } from 'uuid';
 
-
-
 export const Discussionpost = (props) => {
-    const [userslikedPost, setUserslikedPost] = useState([]);   // usersID >>> send to firebase
-    const [isLiked, setIsliked] = useState(false);
-    const [replyFlag, setReplyFlag] = useState(false);
-    const [commentFlag, setCommentFlag] = useState(false);
-    const [comments, setComments] = useState([]);
-    const CommentInputRef = useRef();
+	const [userslikedPost, setUserslikedPost] = useState([]); // usersID >>> send to firebase
+	const [isLiked, setIsliked] = useState(false);
+	const [replyFlag, setReplyFlag] = useState(false);
+	const [commentFlag, setCommentFlag] = useState(false);
+	const [comments, setComments] = useState([]);
+	const CommentInputRef = useRef();
 
-    const [postOwner, setpostOwner] = useState({});
-    const { currentUser } = useContext(AuthContext); 
+	const [postOwner, setpostOwner] = useState({});
+	const { currentUser } = useContext(AuthContext);
 
-    // function: handle like action
-    const handleLike = async() => {
-        // isLiked? setIsliked(false): setIsliked(true);
-        if (isLiked){
-            setIsliked(false);
-            // console.log(currentUser.uid)
-            //     await updateDoc(doc(db,'courseDiscussion', props.courseId),{
-            //        [props.post.discussionId]:
-            //          {        // post
-            //         ...props.post.discussionId,      
+	// function: handle like action
+	const handleLike = async () => {
+		// isLiked? setIsliked(false): setIsliked(true);
+		if (isLiked) {
+			setIsliked(false);
+			// console.log(currentUser.uid)
+			//     await updateDoc(doc(db,'courseDiscussion', props.courseId),{
+			//        [props.post.discussionId]:
+			//          {        // post
+			//         ...props.post.discussionId,
 
-            //             likedBy: arrayRemove(currentUser.uid)
-            //          }
-            //      });
-        }else {
-            setIsliked(true)
+			//             likedBy: arrayRemove(currentUser.uid)
+			//          }
+			//      });
+		} else {
+			setIsliked(true);
 			// await updateDoc(doc(db, 'courseDiscussion', props.courseId), {
-            //     [props.post.discussionId]:
-            //     {  
-            //               // post
-            //         ...props.post.discussionId,      
-            //        likedBy: arrayUnion(currentUser.uid)
-            //     }
+			//     [props.post.discussionId]:
+			//     {
+			//               // post
+			//         ...props.post.discussionId,
+			//        likedBy: arrayUnion(currentUser.uid)
+			//     }
 			// });
-        }  
-    }  
+		}
+	};
 
+	// const handleLike = ()=>{
+	//     isLiked? setIsliked(false): setIsliked(true);
+	// }
+	// function: handle reply action
+	const handleReply = () => {
+		setReplyFlag(true);
+	};
+	// function: comment input
+	const addCommnetValue = () => {
+		let commentsArr = comments;
+		if (CommentInputRef.current.value.trim() != '') {
+			commentsArr.push(CommentInputRef.current.value.trim());
+			setComments([...commentsArr]);
+			setReplyFlag(false);
+			setCommentFlag(true);
+			console.log(comments);
+		}
+		handleCommentSubmit();
+	};
 
-
-    // const handleLike = ()=>{
-    //     isLiked? setIsliked(false): setIsliked(true);
-    // }
-    // function: handle reply action
-    const handleReply = ()=>{
-        setReplyFlag(true);
-    }
-    // function: comment input
-    const addCommnetValue = ()=>{
-        let commentsArr = comments;
-        if(CommentInputRef.current.value.trim() != ""){
-            commentsArr.push(CommentInputRef.current.value.trim());
-            setComments([...commentsArr]);
-            setReplyFlag(false);
-            setCommentFlag(true);
-            console.log(comments);
-        }
-        handleCommentSubmit();
-    }
-
-    useEffect (() =>{
-        const getOwnerData = onSnapshot(doc(db, 'users', props.post.createdBy), (doc) => {
-            console.log(doc.data())
-            setpostOwner(doc.data());
-        });
-        return () => {
+	useEffect(() => {
+		const getOwnerData = onSnapshot(doc(db, 'users', props.post.createdBy), (doc) => {
+			console.log(doc.data());
+			setpostOwner(doc.data());
+		});
+		return () => {
 			getOwnerData();
 		};
-    },[props.post])
-    console.log(postOwner)
+	}, [props.post]);
+	console.log(postOwner);
 
-    const clean = ()=>{
-        CommentInputRef.current.value = "";
-    };
+	const clean = () => {
+		CommentInputRef.current.value = '';
+	};
 
-// function to send comments to firebase
-    const handleCommentSubmit = async () => {
-        const discussionCommentId = uuid();
-        try {
-            if (CommentInputRef.current.value.trim() !== ''){
-                let inputvalue = CommentInputRef.current.value.trim();
-                console.log(currentUser.uid);
-                await updateDoc(doc(db,'discussionComments', props.post.discussionId),{
-                   "comments":arrayUnion(
-                    {        // comment
-                        discussionId : props.post.discussionId,
-                        commentID: discussionCommentId,
-                        commentContent: inputvalue,
-                        createdBy: currentUser.uid,
-                        createdAt: Timestamp.now(),
-                    }
-                   )
-                });
+	// function to send comments to firebase
+	const handleCommentSubmit = async () => {
+		const discussionCommentId = uuid();
+		try {
+			if (CommentInputRef.current.value.trim() !== '') {
+				let inputvalue = CommentInputRef.current.value.trim();
+				console.log(currentUser.uid);
+				await updateDoc(doc(db, 'discussionComments', props.post.discussionId), {
+					comments: arrayUnion({
+						// comment
+						discussionId: props.post.discussionId,
+						commentID: discussionCommentId,
+						commentContent: inputvalue,
+						createdBy: currentUser.uid,
+						createdAt: Timestamp.now(),
+					}),
+				});
 
-                clean();
-            }
-        } catch(error){
-            console.error(error);
-        }
-    }
+				clean();
+			}
+		} catch (error) {
+			console.error(error);
+		}
+	};
 
-    // function to render all post comments
-    const [allComments, setAllComments] = useState([]);
+	// function to render all post comments
+	const [allComments, setAllComments] = useState([]);
 	useEffect(() => {
-		const discPostCommentsData = onSnapshot(doc(db,'discussionComments',props.post.discussionId),(doc) => {
-            // console.log(doc.data()['discussions'])
-            let commentsArr = [];
-            doc.data()['comments'].forEach(element => {
-                commentsArr.push(element); 
-            });
-            setAllComments(commentsArr);
-	
+		const discPostCommentsData = onSnapshot(doc(db, 'discussionComments', props.post.discussionId), (doc) => {
+			// console.log(doc.data()['discussions'])
+			let commentsArr = [];
+			doc.data()['comments'].forEach((element) => {
+				commentsArr.push(element);
+			});
+			setAllComments(commentsArr);
 		});
 		return () => {
 			discPostCommentsData();
 		};
 	}, []);
 
+	console.log(props.post.discussionId);
 
-    console.log(props.post.discussionId);
+	return (
+		<div className='discussion-post mb-3 py-3 mx-auto '>
+			<div className='row mb-2 d-flex align-items-center gx-0'>
+				<div className='col-2 text-center'>
+					<img src={postOwner.photoURL} alt='' />
+				</div>
+				<div className='col-5 d-flex align-items-center flex-column ms-0'>
+					<p className='mb-1 w-100 fw-bold'>{postOwner.displayName}</p>
+				</div>
+			</div>
+			<div className='mb-3 post-body'>
+				<p className='w-100'>{props.post.discussionContent}</p>
+				<div className='d-flex align-items-center w-100'>
+					{isLiked ? (
+						<p className='pe-4 mb-0 interact-icon text-primary' onClick={handleLike}>
+							Liked
+						</p>
+					) : (
+						<p className='pe-4 mb-0 interact-icon' onClick={handleLike}>
+							Like
+						</p>
+					)}
+					<p className='pe-2 mb-0 interact-icon' onClick={handleReply}>
+						Reply
+					</p>
+				</div>
+			</div>
+			{replyFlag && (
+				<div className='reply mx-auto py-2 px-3'>
+					<div className='row'>
+						<div className='col-2'>
+							<BsArrowReturnRight className='reply-arrow' />
+						</div>
+						<div className='col-9 reply-input pt-2'>
+							<div className='row'>
+								<div className='col-2 text-center px-1'>
+									<img src={currentUser?.photoURL} alt='' />
+								</div>
+								<div className='col-8 d-flex align-items-center flex-column'>
+									<p className='mb-1 w-100 fw-bold'>{currentUser?.displayName}</p>
+									{currentUser?.specialFlags.isInstructor && <img className='inst-badge' src={Instructor} alt='Instructor' title='Instructor' />}
+									<div className='w-100'>
+										<textarea type='text' className='input-text w-100 py-1 px-2' maxLength={100} ref={CommentInputRef} />
+									</div>
+								</div>
+								<div className='col-1 text-end d-flex flex-column-reverse pb-4'>
+									<AiOutlineSend className='fs-5 submit-icon' onClick={addCommnetValue} />
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			)}
 
-    return (
-        <div className="discussion-post mb-3 py-3 mx-auto ">
-            <div className='row mb-2 d-flex align-items-center gx-0'>
-                <div className='col-2 text-center'>
-                    <img src={postOwner.photoURL} alt="" />
-                </div>
-                <div className='col-5 d-flex align-items-center flex-column ms-0'>
-                    <p className='mb-1 w-100 fw-bold'>{postOwner.displayName}</p>
-                </div>
-            </div>
-            <div className='mb-3 post-body'>
-                <p className='w-100'>{props.post.discussionContent}</p>
-                    <div className='d-flex align-items-center w-100'>
-                        {
-                            isLiked?
-                            <p className='pe-4 mb-0 interact-icon text-primary' onClick={handleLike}>Liked</p>
-                            :<p className='pe-4 mb-0 interact-icon' onClick={handleLike}>Like</p>
-                        }
-                        <p className='pe-2 mb-0 interact-icon' onClick={handleReply}>Reply</p>
-                    </div>
-            </div>
-            {
-                replyFlag &&
-                <div className='reply mx-auto py-2 px-3'> 
-                    <div className="row">
-                        <div className="col-2">
-                            <BsArrowReturnRight className='reply-arrow'/>
-                        </div>
-                        <div className="col-9 reply-input pt-2">
-                            <div className='row'>
-                                <div className='col-2 text-center px-1'>
-                                    <img src={currentUser?.photoURL} alt="" />
-                                </div>
-                                <div className='col-8 d-flex align-items-center flex-column'>
-                                    <p className='mb-1 w-100 fw-bold'>{currentUser?.displayName}</p>
-                                    <div className='w-100'>
-                                    <textarea 
-                                        type="text" 
-                                        className="input-text w-100 py-1 px-2"
-                                        maxLength={100}
-                                        ref ={CommentInputRef} />
-                                    </div>
-                                </div>
-                                <div className="col-1 text-end d-flex flex-column-reverse pb-4">
-                                    <AiOutlineSend className="fs-5 submit-icon" onClick={addCommnetValue}/>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            }
-            
-                <div className='comments'>
-                    {
-                        allComments.map((comment, i)=>{
-                            return(
-                                <div key={i}>
-                                    <Discussioncomment comment = {comment}/>
-                                </ div>
-                            )
-                        })
-                    }
-
-                </div>
-            
-        </div>
-    )
-}
+			<div className='comments'>
+				{allComments.map((comment, i) => {
+					return (
+						<div key={i}>
+							<Discussioncomment comment={comment} />
+						</div>
+					);
+				})}
+			</div>
+		</div>
+	);
+};
